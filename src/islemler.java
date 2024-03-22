cef3cb3dcd1b77169e046e1b48727d5ba3ff5a3b
Kadir Can Geyik <kadircangeyik2001@gmail.com>
2024-03-23 01:49:24 +0300

    Update islemler.java

--- src/islemler.java
+++ src/islemler.java
@@ -47,39 +47,88 @@ public class islemler {
         sonuc = sayi1 / sayi2;
         System.out.println("Sonuc: " + formatSonuc(sonuc));
     }
+        //Samet
+        public static void turev() {
+            System.out.println("Türev Hesaplama İşlemine Hoş Geldiniz!");
+            System.out.println("Fonksiyonu ax^n şeklinde giriniz.");
+            System.out.print("a değeri: ");
+            double a = scanner.nextDouble();
+            System.out.print("n değeri: ");
+            double n = scanner.nextDouble();
+            System.out.print("x değeri: ");
+            double x = scanner.nextDouble();
 
-    //Samet
-    public static void turev() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("f(x)=c.x^n türevini alma programı ");
-        System.out.println("katsayi giriniz (c) = ");
-        int katsayi = input.nextInt();
-        if (katsayi == 0) {
-            System.out.println("f(x)=" + katsayi + "*x^n ise f(x)=0");
-            System.out.println("f'(x)=0");
-        } else {
-            System.out.println("us giriniz(n)=");
-            int us = input.nextInt();
-            System.out.println("f(x)=" + katsayi + "*x^" + us);
-            System.out.println("f'(x)=" + (katsayi * us) + "*x^" + (us - 1));
-
-        }
+            double sonuc = hesaplaTurev(a, n, x);
+            System.out.println("Sonuç: " + sonuc);
     }
-
-
+        public static double hesaplaTurev(double a, double n, double x){
+            // Türev formülü: n * a * x^(n-1)
+            double turevSonuc = n * a * Math.pow(x, n - 1);
+            return turevSonuc;
+        }
     //Serhat
     public static void integral() {
+        System.out.println("Integral Hesaplama İşlemine Hoş Geldiniz!");
+        System.out.println("Fonksiyonu ax^n şeklinde giriniz.");
+        System.out.print("a değeri: ");
+        double a = scanner.nextDouble();
+        System.out.print("n değeri: ");
+        double n = scanner.nextDouble();
+        System.out.print("x değeri: ");
+        double x = scanner.nextDouble();
 
+        double sonuc = hesaplaIntegral(a, n, x);
+        System.out.println("Sonuç: " + sonuc);
     }
 
-    //Kadir
+    public static double hesaplaIntegral(double a, double n, double x) {
+        // İntegral formülü: (a/(n+1)) * x^(n+1)
+        double integralSonuc = (a / (n + 1)) * Math.pow(x, n + 1);
+        return integralSonuc;
+    }
     public static void fonksiyon() {
+        System.out.println("Fonksiyon Hesaplama İşlemine Hoş Geldiniz!");
+        System.out.println("Fonksiyonu ax^2 + bx + c şeklinde giriniz.");
+        System.out.print("a değeri: ");
+        double a = scanner.nextDouble();
+        System.out.print("b değeri: ");
+        double b = scanner.nextDouble();
+        System.out.print("c değeri: ");
+        double c = scanner.nextDouble();
+        System.out.print("x değeri: ");
+        double x = scanner.nextDouble();
 
+        double sonuc = hesaplaFonksiyon(a, b, c, x);
+        System.out.println("Sonuç: " + sonuc);
     }
-
+    public static double hesaplaFonksiyon(double a, double b, double c, double x) {
+        return a * Math.pow(x, 2) + b * x + c;
+    }
     //Kaan
     public static void parabol() {
+        System.out.print("X1'i giriniz: ");
+        double x1 = scanner.nextDouble();
+        System.out.print("Y1'i giriniz: ");
+        double y1 = scanner.nextDouble();
 
+        System.out.print("X2'yi giriniz: ");
+        double x2 = scanner.nextDouble();
+        System.out.print("Y2'yi giriniz: ");
+        double y2 = scanner.nextDouble();
+
+        System.out.print("X3'ü giriniz: ");
+        double x3 = scanner.nextDouble();
+        System.out.print("Y3'ü giriniz: ");
+        double y3 = scanner.nextDouble();
+
+        // Lagrange polinomu kullanarak katsayıları hesapla
+        double a = y1 / ((x1 - x2) * (x1 - x3)) + y2 / ((x2 - x1) * (x2 - x3)) + y3 / ((x3 - x1) * (x3 - x2));
+        double b = -y1 * (x2 + x3) / ((x1 - x2) * (x1 - x3)) - y2 * (x1 + x3) / ((x2 - x1) * (x2 - x3)) - y3 * (x1 + x2) / ((x3 - x1) * (x3 - x2));
+        double c = y1 * x2 * x3 / ((x1 - x2) * (x1 - x3)) + y2 * x1 * x3 / ((x2 - x1) * (x2 - x3)) + y3 * x1 * x2 / ((x3 - x1) * (x3 - x2));
+
+        // Parabol denklemi ve tepe noktasını yazdır
+        System.out.println("Kuadratik: y = (" + a + ")x^2 + (" + b + ")x + (" + c + ")");
+        System.out.println("Tepe noktası: (x - " + (-b / (2 * a)) + ")^2 + " + ((4 * a * c - b * b) / (4 * a)));
     }
     public static String formatSonuc(double sonuc) {
         String formattedSonuc = String.valueOf(sonuc);
